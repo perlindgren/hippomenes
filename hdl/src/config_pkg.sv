b7c702e640dbda0cfaa8265f3a821e00a4c14627
Per Lindgren <per.lindgren@ltu.se>
2024-02-21 08:54:51 +0100

    polish

--- hdl/src/config_pkg.sv
+++ hdl/src/config_pkg.sv
@@ -6,7 +6,6 @@ package config_pkg;
   parameter integer IMemStart = 'h0000_0000;
   parameter integer IMemSize = 'h0000_1000;
 
-
   // Data memory configuration
   parameter integer DMemStart = 'h0001_0000;
   parameter integer DMemSize = 'h0000_1000;
