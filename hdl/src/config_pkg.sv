20e739e67d1be842488f0ae7b4f691c905e82465
Per Lindgren <per.lindgren@ltu.se>
2024-03-06 13:27:59 +0100

    package

--- hdl/src/config_pkg.sv
+++ hdl/src/config_pkg.sv
@@ -4,12 +4,12 @@
 package config_pkg;
 
   // Instruction memory configuration
-  parameter integer unsigned IMemStart = 'h0000_0000;
-  parameter integer unsigned IMemSize = 'h0000_0100;  // in bytes
+  localparam integer unsigned IMemStart = 'h0000_0000;
+  localparam integer unsigned IMemSize = 'h0000_0100;  // in bytes
 
   // Data memory configuration
-  parameter integer unsigned DMemStart = 'h0001_0000;
-  parameter integer unsigned DMemSize = 'h0000_1000;  // in bytes
+  localparam integer unsigned DMemStart = 'h0001_0000;
+  localparam integer unsigned DMemSize = 'h0000_1000;  // in bytes
 
   localparam integer unsigned IMemAddrWidth = $clog2(IMemSize);  // derived
   localparam integer unsigned DMemAddrWidth = $clog2(DMemSize);  // derived
