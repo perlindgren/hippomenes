c4e9de0f86bb34f688db8ea5251203e36854dca1
Per Lindgren <per.lindgren@ltu.se>
2024-03-09 23:17:56 +0100

    auto clear pending bit

--- hdl/src/core/n_clic.sv
+++ hdl/src/core/n_clic.sv
@@ -88,22 +88,14 @@ module n_clic
 
   typedef logic [(IMemAddrWidth - 2)-1:0] IMemAddrStore;
 
-  /* verilator lint_off UNOPTFLAT */
-  PrioT         max_prio    [VecSize];
-  IMemAddrStore max_vec     [VecSize];
-  // logic         is_int      [VecSize];
-  entry_t       entry       [VecSize];
-  PrioT         prio        [VecSize];
-  IMemAddrStore csr_vec_data[VecSize];
-
+  entry_t                            entry           [VecSize];
+  PrioT                              prio            [VecSize];
+  IMemAddrStore                      csr_vec_data    [VecSize];
+  logic                              ext_write_enable[VecSize];
+  logic         [$bits(entry_t)-1:0] ext_entry_data;
   generate
-    word temp_vec[VecSize];
+    word temp_vec  [VecSize];
     word temp_entry[VecSize];
-    //r interrupt;
-    // automatically connected
-    logic ext_write_enable;
-    logic [(IMemAddrWidth - 2)-1:0] ext_vec_data;
-    logic [$bits(entry_t)-1:0] ext_entry_data;
 
     for (genvar k = 0; k < VecSize; k++) begin : gen_vec
       csr #(
@@ -118,8 +110,8 @@ module n_clic
           .csr_op(csr_op),
           .rs1_zimm(rs1_zimm),
           .rs1_data(rs1_data),
-          .ext_write_enable,
-          .ext_data(ext_vec_data),
+          .ext_write_enable(0),
+          .ext_data(0),
           // out
           .out(temp_vec[k])
       );
@@ -136,53 +128,56 @@ module n_clic
           .csr_op(csr_op),
           .rs1_zimm(rs1_zimm),
           .rs1_data(rs1_data),
-          .ext_write_enable,
+          .ext_write_enable(ext_write_enable[k]),
           .ext_data(ext_entry_data),
           // out
           .out(temp_entry[k])
       );
 
-      assign entry[k]         = entry_t'(temp_entry[k]);  // gen_vec[k].csr_entry.data;
-      assign prio[k]          = entry[k].prio;  // a bit of a hack to please Verilator
-      assign csr_vec_data[k]  = IMemAddrStore'(temp_vec[k]);  // gen_vec[k].csr_vec.data;
-
-      assign ext_write_enable = 0;  // these should not be written as of now
-      assign ext_vec_data     = 0;  // these should not be written as of now
-      assign ext_entry_data   = 0;  // these should not be written as of now
+      assign entry[k]        = entry_t'(temp_entry[k]);  // gen_vec[k].csr_entry.data;
+      assign prio[k]         = entry[k].prio;  // a bit of a hack to please Verilator
+      assign csr_vec_data[k] = IMemAddrStore'(temp_vec[k]);  // gen_vec[k].csr_vec.data;
     end
-
   endgenerate
 
   // stupid implementation to find max priority
+  /* verilator lint_off UNOPTFLAT */
+  PrioT         max_prio [VecSize];
+  IMemAddrStore max_vec  [VecSize];
+  VecT          max_index[VecSize];
   always_comb begin
     for (integer k = 0; k < VecSize; k++) begin
       if (k == 0) begin
         if (entry[k].enabled && entry[k].pended && (prio[k] >= m_int_thresh.data)) begin
-          // is_int[0]   = 1;
-          max_prio[0] = prio[k];
-          max_vec[0]  = csr_vec_data[k];
+          max_prio[0]  = prio[k];
+          max_vec[0]   = csr_vec_data[k];
+          max_index[0] = 0;
         end else begin
-          // is_int[0]   = 0;
-          max_prio[0] = m_int_thresh.data;
-          max_vec[0]  = 0;
+          max_prio[0]  = m_int_thresh.data;
+          max_vec[0]   = 0;
+          max_index[k] = 0;
         end
       end else begin
         if (entry[k].enabled && entry[k].pended && (prio[k] >= max_prio[k-1])) begin
-          // is_int[k]   = 1;
-          max_prio[k] = prio[k];
-          max_vec[k]  = csr_vec_data[k];
+          max_prio[k]  = prio[k];
+          max_vec[k]   = csr_vec_data[k];
+          max_index[k] = VecT'(k);
         end else begin
-          // is_int[k]   = is_int[k-1];
-          max_prio[k] = max_prio[k-1];
-          max_vec[k]  = max_vec[k-1];
+          max_prio[k]  = max_prio[k-1];
+          max_vec[k]   = max_vec[k-1];
+          max_index[k] = max_index[k-1];
         end
       end
     end
   end
 
   always_comb begin
+    ext_write_enable = '{default: '0};  // we don't touch the csr:s by default
+    ext_entry_data   = entry[max_index[VecSize-1]] & ~1;  // clear pend bit
     // check return from interrupt condition
-    if (pc_in == ~(IMemAddrWidth'(0)) && (max_prio[VecSize-1] >= m_int_thresh.data)) begin
+    if ((pc_in == ~(IMemAddrWidth'(0))) &&
+        entry[max_index[VecSize-1]].enabled && entry[max_index[VecSize-1]].pended &&
+        (max_prio[VecSize-1] >= m_int_thresh.data)) begin
       // tail chaining, not sure this is correct
       push = 0;
       pop = 0;
@@ -191,6 +186,7 @@ module n_clic
       m_int_thresh_write_enable = 0;  // no update of threshold
       interrupt_out = 1;
       pc_interrupt_sel = PC_INTERRUPT;
+      ext_write_enable[max_index[VecSize-1]] = 1;  // write to pended interrupt
       $display("tail chaining level_out %d, pop %d", level_out, pop);
     end else if (max_prio[VecSize-1] > m_int_thresh.data) begin
       push = 1;
@@ -200,6 +196,7 @@ module n_clic
       m_int_thresh_write_enable = 1;
       interrupt_out = 1;
       pc_interrupt_sel = PC_INTERRUPT;
+      ext_write_enable[max_index[VecSize-1]] = 1;  // write to pended interrupt
       $display("interrupt take int_addr %d", int_addr);
     end else if (pc_in == ~(IMemAddrWidth'(0))) begin
       push = 0;
@@ -209,7 +206,7 @@ module n_clic
       m_int_thresh_write_enable = 1;
       interrupt_out = 0;
       pc_interrupt_sel = PC_INTERRUPT;
-      // $display("pop");
+      $display("interrupt return");
     end else begin
       push = 0;
       pop = 0;
