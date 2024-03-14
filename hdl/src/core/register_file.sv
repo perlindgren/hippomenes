ae3a287a5e9b5a65e408708b0e1c482df7fdc95a
Per Lindgren <per.lindgren@ltu.se>
2024-03-14 15:11:41 +0100

    wip low level reg file

--- hdl/src/core/register_file.sv
+++ hdl/src/core/register_file.sv
@@ -2,12 +2,102 @@
 
 module register_file
   import config_pkg::*;
-#(
-    parameter bit DummyInstructions = 0,
-    parameter bit WrenCheck         = 0,
-    parameter bit RdataMuxCheck     = 0,
+(
+    // Clock and Reset
+    input logic clk_i,
+    input logic rst_ni,
 
-    localparam RegT RegZeroVal = '0
+    // Read port R1
+    input RegAddrT raddr_a_i,
+    output RegT rdata_a_o,
+    // Read port R2
+    input RegAddrT raddr_b_i,
+    output RegT rdata_b_o,
+    // Write port W1
+    input RegAddrT waddr_a_i,
+    input RegT wdata_a_i,
+    input logic we_a_i,
+    // Set the ra register to magic number
+    input logic ra_set
+);
+
+  RegT  x3_31_a_o;
+  RegT  x3_31_b_o;
+  logic x3_31_we;
+
+  rf #(
+      .RegNum(RegNum - 3)
+  ) x3_x31 (
+      // Clock and Reset
+      .clk_i,
+      .rst_ni,
+      // Read port R1
+      .raddr_a_i(raddr_a_i - 3),
+      .rdata_a_o(x3_31_a_o),
+      // Read port R2
+      .raddr_b_i(raddr_b_i - 3),
+      .rdata_b_o(x3_31_b_o),
+      // Write port W1
+      .waddr_a_i(waddr_a_i - 3),
+      .wdata_a_i,
+      .we_a_i(x3_31_we)
+  );
+
+  RegT  ra_a_o;
+  RegT  ra_b_o;
+  logic ra_we;
+  RegT  ra_wdata;
+
+  rf #(
+      .RegNum(1)
+  ) ra (
+      // Clock and Reset
+      .clk_i,
+      .rst_ni,
+      // Read port R1
+      .raddr_a_i(0),
+      .rdata_a_o(ra_a_o),
+      // Read port R2
+      .raddr_b_i(0),
+      .rdata_b_o(ra_b_o),
+      // Write port W1
+      .waddr_a_i(0),
+      .wdata_a_i(ra_wdata),
+      .we_a_i(ra_we)
+  );
+
+  always_comb begin
+    if (raddr_a_i == Ra) begin
+      $display("raddr_a_i == Ra");
+      rdata_a_o = ra_a_o;
+    end else rdata_a_o = x3_31_a_o;
+
+    if (raddr_b_i == Ra) begin
+      $display("raddr_b_i == Ra");
+      rdata_b_o = ra_b_o;
+    end else rdata_b_o = x3_31_b_o;
+
+    x3_31_we = we_a_i && (waddr_a_i > Sp);
+    ra_we = (we_a_i && (waddr_a_i == Ra)) || ra_set;
+    if (ra_set) begin
+      $display("ra_wdata = ~0");
+      ra_wdata = ~0;
+    end else begin
+      ra_wdata = wdata_a_i;
+    end
+
+    $display("x3_31_we %b, ra_we %b", x3_31_we, ra_we);
+
+  end
+
+endmodule
+
+module rf
+  import config_pkg::*;
+#(
+    parameter integer unsigned RegNum = 32,
+    localparam integer unsigned RegAddrWidth = ($clog2(RegNum) > 0) ? $clog2(RegNum) : 1,
+    localparam type RegAddrT = logic [RegAddrWidth-1:0]
 ) (
     // Clock and Reset
     input logic clk_i,
@@ -26,28 +116,23 @@ module register_file
 );
 
   RegT  mem[RegNum];
-  // DataT mem_o_a, mem_o_b;
   logic we;
 
   assign we = we_a_i;
   assign rdata_a_o = mem[raddr_a_i];
   assign rdata_b_o = mem[raddr_b_i];
 
-  // Note that the SystemVerilog LRM requires variables on the LHS of assignments within
-  // "always_ff" to not be written to by any other process. However, to enable the initialization
-  // of the inferred RAM32M primitives with non-zero values, below "initial" procedure is needed.
-  // Therefore, we use "always" instead of the generally preferred "always_ff" for the synchronous
-  // write procedure.
-  always @(posedge clk_i) begin : sync_write
-    if (we == 1'b1 && waddr_a_i != 0) begin
+  always_ff @(posedge clk_i) begin : sync_write
+    if (we == 1'b1) begin
+      $display("<< -- write -- >>");
       mem[waddr_a_i] <= wdata_a_i;
     end
   end : sync_write
 
-  // Make sure we initialize the BRAM with the correct register reset value.
+  // maybe this will if implemented as native ram blocks
   initial begin
     for (int k = 0; k < RegNum; k++) begin
-      mem[k] = RegZeroVal;
+      mem[k] = 0;
     end
   end
 
--- hdl/src/core/tb_register_file.sv
+++ hdl/src/core/tb_register_file.sv
@@ -2,39 +2,37 @@
 `timescale 1ns / 1ps
 
 module tb_register_file;
-  parameter integer unsigned DataWidth = 32;
-  parameter integer unsigned NumRegs = 32;
-  parameter integer unsigned IndexWidth = $clog2(NumRegs);
-  localparam type DataT = logic [DataWidth-1:0];
-  localparam int unsigned AddrWidth = 5;
-  localparam type AddrT = logic [AddrWidth-1:0];
-
+  import config_pkg::*;
 
   logic clk_i;
   logic rst_ni;
+
   logic we_a_i;
-  AddrT waddr_a_i;
-  DataT wdata_a_i;
-  AddrT raddr_a_i;
-  AddrT raddr_b_i;
-  DataT rdata_a_o;
-  DataT rdata_b_o;
+  RegAddrT waddr_a_i;
+  RegT wdata_a_i;
+  RegAddrT raddr_a_i;
+  RegAddrT raddr_b_i;
+  RegT rdata_a_o;
+  RegT rdata_b_o;
+  logic ra_set;
 
   register_file dut (
       // Clock and Reset
       .clk_i,
       .rst_ni,
 
-      //Read port R1
+      // Read port R1
       .raddr_a_i,
       .rdata_a_o,
-      //Read port R2
+      // Read port R2
       .raddr_b_i,
       .rdata_b_o,
       // Write port W1
       .waddr_a_i,
       .wdata_a_i,
-      .we_a_i
+      .we_a_i,
+      // Set the ra register to magic number
+      .ra_set
   );
 
   always #10 clk_i = ~clk_i;
@@ -51,32 +49,79 @@ module tb_register_file;
     rst_ni = 0;
     #10;
 
-    raddr_a_i = 0;
-    raddr_b_i = 1;
+    ra_set = 0;
+    raddr_a_i = 3;
+    raddr_b_i = 4;
     we_a_i = 1;
-    waddr_a_i = 1;
+    waddr_a_i = 4;
     wdata_a_i = 'h12345678;
 
     #20;
     assert ((rdata_a_o == 0) && (rdata_b_o == 'h12345678)) $display("ok");
     else $error("rs1 %h, rs2 %h", rdata_a_o, rdata_b_o);
 
-    assert ((rdata_a_o == 0) && (rdata_b_o == 'h12345678)) $display("ok");
+    raddr_a_i = 4;
+    raddr_b_i = 3;
+
+    #1;  // pass time
+
+    assert ((rdata_b_o == 0) && (rdata_a_o == 'h12345678)) $display("ok");
     else $error("rs1 %h, rs2 %h", rdata_a_o, rdata_b_o);
 
     $display("we_a_i %b waddr_a_i %d wdata_a_i %h", we_a_i, waddr_a_i, wdata_a_i);
     $display("rs1 %h, rs2 %h", rdata_a_o, rdata_b_o);
-    #20;
 
-    assert ((rdata_a_o == 0) && (rdata_b_o == 'h12345678)) $display("ok");
+
+    we_a_i = 1;
+    raddr_a_i = 0;
+    raddr_b_i = Ra;
+    waddr_a_i = Ra;
+    wdata_a_i = 'h1000_0001;
+
+    #20
+
+    assert ((rdata_a_o == 0) && (rdata_b_o == 'h1000_0001)) $display("ok");
     else $error("rs1 %h, rs2 %h", rdata_a_o, rdata_b_o);
 
+    we_a_i = 0;
+    ra_set = 1;
+    we_a_i = 0;
+    raddr_a_i = 0;
+    raddr_b_i = Ra;
+
+    #19;
+
+    assert ((rdata_a_o == 0) && (rdata_b_o == 'hffff_ffff)) $display("ok");
+    else $error("rs1 %h, rs2 %h", rdata_a_o, rdata_b_o);
+
+    we_a_i = 1;
     waddr_a_i = 31;
     wdata_a_i = 'hdead_beef;
     raddr_a_i = 31;
+
     #20;
-    assert ((rdata_a_o == 'hdead_beef) && (rdata_b_o == 'h12345678)) $display("ok");
+    assert ((rdata_a_o == 'hdead_beef) && (rdata_b_o == 'hffff_ffff)) $display("ok");
     else $error("rs1 %h, rs2 %h", rdata_a_o, rdata_b_o);
+
+    we_a_i = 1;
+    ra_set = 0;
+    wdata_a_i = 'hdead_1001;
+    waddr_a_i = Ra;
+
+    #20;
+    assert ((rdata_a_o == 'hdead_beef) && (rdata_b_o == 'hdead_1001)) $display("ok");
+    else $error("rs1 %h, rs2 %h", rdata_a_o, rdata_b_o);
+
+    we_a_i = 1;
+    ra_set = 1;
+    wdata_a_i = 'hdead_1001;
+    waddr_a_i = 31;
+
+    #20;
+
+    assert ((rdata_a_o == 'hdead_1001) && (rdata_b_o == 'hffff_ffff)) $display("ok");
+    else $error("rs1 %h, rs2 %h", rdata_a_o, rdata_b_o);
+
 
     #10 $finish;
 
