6af7acdec9a26461f1db38d57cbd992942d0da46
Per Lindgren <per.lindgren@ltu.se>
2024-03-28 16:54:03 +0100

    wip

---
+++ fpga/program_arty.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+updatemem --meminfo arty/arty.runs/impl_1/fpga_arty.mmi --data ../rust_examples/binary.mem --proc hippo/imem/xpm_memory_spram_inst/xpm_memory_base_inst --bit arty/arty.runs/impl_1/fpga_arty.bit -out arty/arty.runs/impl_1/fpga_arty.bit -force
+openFPGALoader -b  arty arty/arty.runs/impl_1/fpga_arty.bit
+
