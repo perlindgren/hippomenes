1591f40fb3711112ba914476f5b636ea135e3ef3
Per Lindgren <per.lindgren@ltu.se>
2024-04-19 19:21:15 +0200

    update jtag frequency to 20MHz (caps out at 15 on my machine)

--- rust_examples/run.sh
+++ rust_examples/run.sh
@@ -9,4 +9,4 @@ updatemem --bit ../fpga/arty/arty.runs/impl_1/fpga_arty.bit --meminfo ../fpga/ar
  --data ./data_3.mem --proc hippo/dmem/block_3/xpm_memory_spram_inst/xpm_memory_base_inst \
  -out ../fpga/arty/arty.runs/impl_1/fpga_arty.bit -force
 
-openFPGALoader -b  arty ../fpga/arty/arty.runs/impl_1/fpga_arty.bit
+openFPGALoader -b  arty ../fpga/arty/arty.runs/impl_1/fpga_arty.bit --freq 20000000
