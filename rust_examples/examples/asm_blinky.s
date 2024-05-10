41e6c04c41a58b7769af97bbf09f51c01b3e2346
onsdagens <pawdzi-7@student.ltu.se>
2024-05-10 16:20:18 +0200

    blinky blinks again

--- rust_examples/examples/asm_blinky.s
+++ rust_examples/examples/asm_blinky.s
@@ -3,8 +3,8 @@
             .section .init
 init:       la      sp, _stack_start        # set stack pointer
            
-main:       csrwi   0x0, 0b1111  # led-on
-            
+main:       csrwi   0x0, 0b1111  # leds-on
+            csrwi   0x0, 0b0000  # leds-off 
             j       main
 
 data:       .data
